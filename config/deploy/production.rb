6d7537665aea9f919ca4d9789be364edfaf6e7ec
aaabb6211 <jopgfmabb@gmail.com>
2020-03-31 20:43:22 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,7 @@
-server '18.178.132.167', user: 'ec2-user', roles: %w{app db web}
+
+
+
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -60,3 +63,4 @@ server '18.178.132.167', user: 'ec2-user', roles: %w{app db web}
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+server '18.178.132.167', user: 'ec2-user', roles: %w{app db web}
