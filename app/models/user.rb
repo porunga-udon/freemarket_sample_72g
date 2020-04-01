4715a24013f2f317c9819f86f3115908841d181b
hideOz <sabokura@gmail.com>
2020-04-01 15:06:06 +0900

    addValid
    #phone_numberのバリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
   VALID_KANA_REGEX = /\A[ぁ-んァ-ヶー－]+\z/
-
+  VALID_NUMBER_REGEX = /\A[0-9]+\z/
 
   validates :name,               presence: true, uniqueness: true
   validates :email,              presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
@@ -23,4 +23,5 @@ class User < ApplicationRecord
   validates :birth_year,         presence: true
   validates :birth_month,        presence: true
   validates :birth_day,          presence: true
+  validates :phone_number,       format: { with: VALID_NUMBER_REGEX }
 end
