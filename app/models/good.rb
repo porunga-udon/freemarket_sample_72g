347851c7d7688e908cfce1c62a9cdcf885f09308
aaabb6211 <jopgfmabb@gmail.com>
2020-04-01 15:44:22 +0900

    createGoodModelAndDB

---
+++ app/models/good.rb
@@ -0,0 +1,16 @@
+class Good < ApplicationRecord
+
+ # has_many :buyed_goods, foreign_key: "buyer_id", class_name: "Good", dependent: :destroy
+  has_many :saling_goods, -> { where("buyer_id is NULL") }, foreign_key: "saler_id", class_name:  "Good", dependent: :destroy
+  has_many :sold_goods, -> { where("buyer_id is not NULL") }, foreign_key: "saler_id", class_name: "Good", dependent: :destroy
+ # has_many :comments, through: :users_comments, dependent: :destroy
+ # has_many :users_comments, dependent: :destroy
+ # has_many :cards, dependent: :destroy
+ # has_many :orders
+  belongs_to :user_address
+
+VALID_PRICE_REGEX = /\A[0-9]+\z/
+
+  validates :name, presence: true
+  validates :price, presence: true, format:{ with: VALID_PRICE_REGEX }
+end
