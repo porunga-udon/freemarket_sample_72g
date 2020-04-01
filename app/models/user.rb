01e1444517cd03eb329b39a735edc857aee00b43
hideOz <sabokura@gmail.com>
2020-04-01 15:47:59 +0900

    editValid
    #バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  # has_many :buyed_goods, foreign_key: "buyer_id", class_name: "Good", dependent: :destroy
+  # has_many :saling_goods, -> { where("buyer_id is NULL") }, foreign_key: "saler_id", class_name: "Good", dependent: :destroy
+  # has_many :sold_goods, -> { where("buyer_id is not NULL") }, foreign_key: "saler_id", class_name: "Good", dependent: :destroy
+  # has_many :comments, through: :users_comments, dependent: :destroy
+  # has_many :users_comments, dependent: :destroy
+  # has_many :cards, dependent: :destroy
+  # has_many :orders
   belongs_to :user_address
 
 
