f7dbc63e4a3742e7470fa0fe44694fd3aef5d60c
hideOz <sabokura@gmail.com>
2020-04-02 11:55:37 +0900

    editForm
    #ユーザー新規登録機能を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_054559) do
+ActiveRecord::Schema.define(version: 2020_04_02_022056) do
 
   create_table "user_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -44,6 +44,10 @@ ActiveRecord::Schema.define(version: 2020_04_01_054559) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "phone_number"
+    t.string "postcode", null: false
+    t.string "prefecture", null: false
+    t.string "city", null: false
+    t.string "block", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
