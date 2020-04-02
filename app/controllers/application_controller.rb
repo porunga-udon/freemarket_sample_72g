f42823e993b9f5304d486aa9d39d4a9345cb3ee4
hideOz <sabokura@gmail.com>
2020-04-02 18:19:33 +0900

    editApplicationController
    #deviseコントローラーにユーザー情報を登録させるような記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_year])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_month])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_day])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:postcode])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:prefecture])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:city])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:block])
+  end
 end
