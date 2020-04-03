4c6439cbd1b77783d8390bdd078ca20f2d994c03
TatsuyaYokokawa <beattherich@iCloud.com>
2020-04-03 18:24:03 +0900

    TopCssFix

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,11 +1,12 @@
 class GoodsController < ApplicationController
   def index
+    @goods_all = Good.all
     @goods = Good.where(buyer_id: nil).limit(3).order(id: "DESC")
 
     @goods_a = Good.where(buyer_id: nil, category_id: '1').limit(3).order(id: "DESC")
     @goods_b = Good.where(buyer_id: nil, category_id: '2').limit(3).order(id: "DESC")
 
-    @good_images = GoodImage.find_by_sql("SELECT image FROM good_images WHERE good_id IN (SELECT id FROM goods)")
+    @good_images = GoodImage.where(id: @goods_all.ids)
   end
 
   def new
