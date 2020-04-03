8c83e7511b703531c36fb99a02f76a0ae233ce27
aaabb6211 <jopgfmabb@gmail.com>
2020-04-03 13:38:39 +0900

    update

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -2,4 +2,16 @@ class GoodsController < ApplicationController
   def index
     @good = Good.all
   end
+
+  def new
+    @good = Good.new
+   # @category = Category.new
+   # @size = Size.new
+   # @goog_image = Good_image.new
+  end
+
+
+  def show
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'goods#index'
+
+  resources :goods, only:[:index, :show, :new]
 end
