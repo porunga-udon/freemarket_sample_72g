cd5755ae1fd6b911118e3b0458212e465537d592
TatsuyaYokokawa <beattherich@iCloud.com>
2020-04-03 15:15:41 +0900

    fix2

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,5 +1,5 @@
 class Good < ApplicationRecord
-
+#コメントアウト部は後に使用
  # has_many :buyed_goods, foreign_key: "buyer_id", class_name: "Good", dependent: :destroy
   has_many :saling_goods, -> { where("buyer_id is NULL") }, foreign_key: "saler_id", class_name:  "Good", dependent: :destroy
   has_many :sold_goods, -> { where("buyer_id is not NULL") }, foreign_key: "saler_id", class_name: "Good", dependent: :destroy
