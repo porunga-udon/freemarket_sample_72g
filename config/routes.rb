c8aec6fddd8efa60a1dfcff2a81eeb634b4651c5
aaabb6211 <jopgfmabb@gmail.com>
2020-04-04 18:03:25 +0900

    innitial commit
    商品購入ページの実装（フロント）

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :goods, only:[:index, :show, :new] do
     resources :good_images, only: [:index]
   end
+  resources :orders, only:[:index] # 商品購入確認画面へ
 end
