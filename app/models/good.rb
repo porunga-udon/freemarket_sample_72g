34833a1112027b1b32452302f093bea35b8f1456
hideOz <sabokura@gmail.com>
2020-04-08 21:55:30 +0900

    commentOutCategoryAsociation

--- app/models/good.rb
+++ app/models/good.rb
@@ -7,7 +7,7 @@ class Good < ApplicationRecord
   belongs_to :saler, class_name: "User"
   # belongs_to :buyer, class_name: "User"
   # belongs_to :user_address
-  belongs_to :category
+  # belongs_to :category
   has_many :good_images, dependent: :destroy
 
   accepts_nested_attributes_for :good_images, allow_destroy: true
