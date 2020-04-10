033789640333cf7277dfae0a2d8029c12197578e
aaabb6211 <jopgfmabb@gmail.com>
2020-04-10 11:02:20 +0900

    update
    cardテーブルnewとしょw表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'card/new'
+  get 'card/show'
   devise_for :users
   root to: 'goods#index'
   resources :users, only:[:show, :destroy]
-  resources :cards, only:[:new]
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
   resources :goods, only:[:index, :show, :new, :create] do
     resources :good_images, only: [:index, :new, :create]
   end
