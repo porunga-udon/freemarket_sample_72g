db005750f28e798e1ac0708f977494138f28886d
aaabb6211 <jopgfmabb@gmail.com>
2020-04-10 16:52:18 +0900

    update
    マイページカード登録画面に登録フォームを実装（環境変数を用いているため，アビコ以外の環境では調整の必要あり）

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -2,6 +2,11 @@ class CardController < ApplicationController
   require "payjp"
   
   def new
+    @contents = ["お知らせ","やることリスト","いいね！一覧","出品する","下書き一覧","出品した商品-取引中","出品した商品-取引中","出品した商品-売却済み","購入した商品-取引中","購入した商品-過去の取引","ニュース一覧","評価一覧","ガイド","お問い合わせ"]
+    @options = ["プロフィール","発送元・お届先住所変更","支払い方法","メール/パスワード","本人情報","電話番号の確認","ログアウト"]
+    
+    @bland = ["//pcweb-assets.mercdn.net/assets/img/card/visa.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/master-card.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/saison-card.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/jcb.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/american_express.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/dinersclub.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/discover.svg?1152353370"]
+    
     card = Card.where(user_id: current_user.id)
     redirect_to action: "show" if card.exists?
   end
@@ -42,7 +47,7 @@ class CardController < ApplicationController
     if card.blank?
       redirect_to action: "new" 
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
--- db/migrate/20200409051510_create_cards.rb
+++ db/migrate/20200409051510_create_cards.rb
@@ -1,4 +1,4 @@
-class CreateCards < ActiveRecord::Migration[5.2]
+eclass CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
       t.integer :user_id, null:false
