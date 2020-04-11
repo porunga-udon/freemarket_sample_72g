d2b91cfc5fda7df615ff89cc9fb26172da642886
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-04-11 10:54:18 +0900

    add create_hash_method

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,12 +1,8 @@
 class CardController < ApplicationController
   require "payjp"
-  
+  before_action :create_hash
+
   def new
-    @contents = ["お知らせ","やることリスト","いいね！一覧","出品する","下書き一覧","出品した商品-取引中","出品した商品-取引中","出品した商品-売却済み","購入した商品-取引中","購入した商品-過去の取引","ニュース一覧","評価一覧","ガイド","お問い合わせ"]
-    @options = ["プロフィール","発送元・お届先住所変更","支払い方法","メール/パスワード","本人情報","電話番号の確認","ログアウト"]
-    
-    @bland = ["//pcweb-assets.mercdn.net/assets/img/card/visa.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/master-card.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/saison-card.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/jcb.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/american_express.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/dinersclub.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/discover.svg?1152353370"]
-    
     card = Card.where(user_id: current_user.id)
     redirect_to action: "show" if card.exists?
   end
@@ -43,12 +39,6 @@ class CardController < ApplicationController
 
 
   def show  #Cardのデータpayjpに送り情報を取り出します
-    @contents = ["お知らせ","やることリスト","いいね！一覧","出品する","下書き一覧","出品した商品-取引中","出品した商品-取引中","出品した商品-売却済み","購入した商品-取引中","購入した商品-過去の取引","ニュース一覧","評価一覧","ガイド","お問い合わせ"]
-    @options = ["プロフィール","発送元・お届先住所変更","支払い方法","メール/パスワード","本人情報","電話番号の確認","ログアウト"]
-    
-    @bland = ["//pcweb-assets.mercdn.net/assets/img/card/visa.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/master-card.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/saison-card.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/jcb.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/american_express.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/dinersclub.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/discover.svg?1152353370"]
-    
-
     card = Card.where(user_id: current_user.id).first
     if card.blank?
       redirect_to action: "new" 
@@ -58,4 +48,12 @@ class CardController < ApplicationController
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
   end
+
+  def create_hash
+    @contents = ["お知らせ","やることリスト","いいね！一覧","出品する","下書き一覧","出品した商品-取引中","出品した商品-取引中","出品した商品-売却済み","購入した商品-取引中","購入した商品-過去の取引","ニュース一覧","評価一覧","ガイド","お問い合わせ"]
+    @options = ["プロフィール","発送元・お届先住所変更","支払い方法","メール/パスワード","本人情報","電話番号の確認","ログアウト"]
+    
+    @bland = ["//pcweb-assets.mercdn.net/assets/img/card/visa.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/master-card.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/saison-card.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/jcb.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/american_express.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/dinersclub.svg?1152353370","//pcweb-assets.mercdn.net/assets/img/card/discover.svg?1152353370"]
+  end
+
 end
