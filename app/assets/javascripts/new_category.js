c2cd41a8c4106a512457df9393a61b9a46fbaee3
hideOz <sabokura@gmail.com>
2020-04-12 11:55:34 +0900

    Create new_category.js

---
+++ app/assets/javascripts/new_category.js
@@ -0,0 +1,51 @@
+$(function() {
+  var buildPrompt = `<option value>選択してください</option>`
+  var buildHtmlOption = function(cat) {
+    var option = `<option value ="${cat.id}">${cat.name}</option>`
+    return option
+  }
+  $('#parent').change(function() {
+    var parent_id = $(this).val();
+    $.ajax({
+      type: 'GET',
+      url: '/api/child',
+      data: {id: parent_id},
+      dataType: 'json'
+    })
+    .done(function(category) {
+      $('.child').css('display', 'block');
+      $('#child').empty();
+      $('.grand_child').css('display', 'none');
+      $('#child').append(buildPrompt);
+
+      category.forEach(function(cat) {
+        var html_option = buildHtmlOption(cat);
+        $('#child').append(html_option);
+      });
+    })
+    .fail(function(){
+      alert('error');
+    })
+  });
+  $(this).on("change", "#child", function() {
+    var child_id = $("#child").val();
+    $.ajax({
+      type: 'GET',
+      url: '/api/grand_child',
+      data: {id: child_id},
+      dataType: 'json'
+    })
+    .done(function(category) {
+      $('.grand_child').css('display', 'block');
+      $('#grand_child').empty();
+      $('#grand_child').append(buildPrompt);
+      category.forEach(function(cat) {
+        var html_option = buildHtmlOption(cat);
+        $('#grand_child').append(html_option);
+      });
+    })
+    .fail(function() {
+      alert('error');
+    })
+  });
+});
