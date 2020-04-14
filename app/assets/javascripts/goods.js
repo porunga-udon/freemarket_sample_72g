2ff1f0ff4f959c102893300f8ab21ec6970376d9
TatsuyaYokokawa <beattherich@iCloud.com>
2020-04-14 11:52:41 +0900

    Update goods.js

--- app/assets/javascripts/goods.js
+++ app/assets/javascripts/goods.js
@@ -20,6 +20,26 @@ $(document).on('turbolinks:load', function(){
       return html;
     }
 
+    if (window.location.href.match(/\/goods\/\d+\/edit/)){
+      //登録済み画像のプレビュー表示欄の要素を取得する
+      var prevContent = $('.label-content').prev();
+      labelWidth = (620 - $(prevContent).css('width').replace(/[^0-9]/g, ''));
+      $('.label-content').css('width', labelWidth);
+      //プレビューにidを追加
+      $('.preview-box').each(function(index, box){
+        $(box).attr('id', `preview-box__${index}`);
+      })
+      //削除ボタンにidを追加
+      $('.delete-box').each(function(index, box){
+        $(box).attr('id', `delete_btn_${index}`);
+      })
+      var count = $('.preview-box').length;
+      //プレビューが4あるときは、投稿ボックスを消しておく
+      if (count == 4) {
+        $('.label-content').hide();
+      }
+    }
+
     // ラベルのwidth操作
     function setLabel() {
       //プレビューボックスのwidthを取得し、maxから引くことでラベルのwidthを決定
@@ -54,7 +74,7 @@ $(document).on('turbolinks:load', function(){
         //イメージを追加
         $(`#preview-box__${id} img`).attr('src', `${image}`);
         var count = $('.preview-box').length;
-        //プレビューが5個あったらラベルを隠す 
+        //プレビューが4個あったらラベルを隠す 
         if (count == 4) { 
           label_content.hide();
         }
@@ -69,30 +89,45 @@ $(document).on('turbolinks:load', function(){
       }
     });
 
-    // 画像削除
-    $(document).on('click', '.delete-box', function() {
-      var count = $('.preview-box').length;
-      setLabel(count);
-      //item_images_attributes_${id}_image から${id}に入った数字のみを抽出
-      var id = $(this).attr('id').replace(/[^0-9]/g, '');
-      //取得したidに該当するプレビューを削除
-      $(`#preview-box__${id}`).remove();
-      console.log("new")
-      //フォームの中身を削除 
-      $(`#item_images_attributes_${id}_image`).val("");
+    // 新規投稿
+    if ($(`#item_images_attributes_0__destroy`).length == 0) {
+      $(document).on('click', '.delete-box', function() {
+        var count = $('.preview-box').length;
+        setLabel(count);
+        //item_images_attributes_${id}_image から${id}に入った数字のみを抽出
+        var id = $(this).attr('id').replace(/[^0-9]/g, '');
+        //取得したidに該当するプレビューを削除
+        $(`#preview-box__${id}`).remove();
+        console.log("new")
+        //フォームの中身を削除 
+        $(`#item_images_attributes_${id}_image`).val("");
 
-      //削除時のラベル操作
-      var count = $('.preview-box').length;
+        //削除時のラベル操作
+        var count = $('.preview-box').length;
+        //5個めが消されたらラベルを表示
+        if (count == 4) {
+          label_contentshow();
+        }
+        setLabel(count);
+
+        if(id < 4){
+          //削除された際に、空っぽになったfile_fieldをもう一度入力可能にする
+          label_box.attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
+        }
+        })
+    } else {    
+      $(`#item_images_attributes_${id}__destroy`).prop('checked',true);
       //5個めが消されたらラベルを表示
       if (count == 4) {
-        label_contentshow();
+        $('.label-content').show();
       }
-      setLabel(count);
-
-      if(id < 4){
-        //削除された際に、空っぽになったfile_fieldをもう一度入力可能にする
-        label_box.attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
+      //ラベルのwidth操作
+      setLabel();
+      //ラベルのidとforの値を変更
+      //削除したプレビューのidによって、ラベルのidを変更する
+      if(id < 5){
+        $('.label-box').attr({id: `label-box--${id}`,for: `item_images_attributes_${id}_image`});
       }
-    });
+    }    
   });
 })
