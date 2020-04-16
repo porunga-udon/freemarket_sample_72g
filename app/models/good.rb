c97d69d5405de9a358f27f740c0c2c0922eb5c8c
TatsuyaYokokawa <beattherich@iCloud.com>
2020-04-16 18:34:52 +0900

    Update good.rb

--- app/models/good.rb
+++ app/models/good.rb
@@ -17,7 +17,21 @@ class Good < ApplicationRecord
   VALID_PRICE_REGEX = /\A[0-9]+\z/
 
   validates :name, presence: true
+  validates :state, presence: true
+  validates :region, presence: true
+  validates :postage, presence: true
+  validates :expanation, presence: true
+  validates :shipping_date, presence: true
+  validates :delivery_method_id, presence: true
+  validates :trading_conditions, presence: true
   validates :price, presence: true, format:{ with: VALID_PRICE_REGEX }
+  validates :category_id, presence: true
+  validates :size_id, presence: true
+  validates :buyer, presence: true
+  validates :seller, presence: true
+
+
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :size
   belongs_to_active_hash :delivery_method
