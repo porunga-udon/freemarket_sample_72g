ca9ec57eceb5d66b1b7fbe3dbb57624d20f5ce36
TatsuyaYokokawa <beattherich@iCloud.com>
2020-04-16 19:00:17 +0900

    Update goods.rb

--- spec/factories/goods.rb
+++ spec/factories/goods.rb
@@ -1,19 +1,17 @@
 FactoryBot.define do
 
-  factory good do
-    name               {"ああ"}  
-    state              {"未使用に近い"}  
-    region             {"東京"}  
-    postage            {"送料込み(出品者負担)"}  
-    expanation         {"aaaaaa"}  
-    shipping_date      {"1~2日で発送"}  
-    delivery_method_id {"ああ"}  
-    trading_conditions {"未発送"}  
-    price              {"222"}  
-    category_id        {"1"}  
-    size_id            {"1"}  
-    buyer              {"1"}  
-    seller             {"1"}  
+  factory :good do
+    id                    {1}
+    name                  {"あああ"}
+    state                 {"未使用に近い"}
+    region                {"東京"}
+    postage               {"着払い(購入者負担)"}
+    expanation            {"本当は売りたくありません"}
+    shipping_date         {"1~2日で発送"}
+    delivery_method_id    {2}
+    price                 {890}
+    category_id           {50}   
+    size_id               {3}
+    seller_id             {1}
   end
 end
-  
