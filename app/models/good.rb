2a23496f1e6ffed0f9651675e58055fab8141af0
TatsuyaYokokawa <beattherich@iCloud.com>
2020-04-18 18:29:06 +0900

    Update good.rb

--- app/models/good.rb
+++ app/models/good.rb
@@ -25,8 +25,8 @@ class Good < ApplicationRecord
   validates :shipping_date,         presence: true
   validates :delivery_method_id,    presence: true
   validates :category_id,           presence: true
-  validates :seller_id,             presence: true
-
+  validates :seller_id,             presence: true 
+  validates :good_images,           presence: true
 
 
   extend ActiveHash::Associations::ActiveRecordExtensions
