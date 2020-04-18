035aa7546d383c4357f378c64fe748ca7d0abe6f
TatsuyaYokokawa <beattherich@iCloud.com>
2020-04-18 10:36:56 +0900

    Update goods_controller_spec.rb

--- spec/controllers/goods_controller_spec.rb
+++ spec/controllers/goods_controller_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
-describe GoodsController do
+RSpec.describe GoodsController, type: :controller do
+  let(:user){create(:user)}
+  let(:category){create(:category)}
   describe 'GET #edit' do
     it "@goodに正しい値が入っていること" do
-      good = create(:good)
-      get :edit, params: { id: good }
+      good = create(:good, seller_id: user.id, category_id: category.id)
+      get :edit, params: { id: good}
       expect(assigns(:good)).to eq good
     end
 
     it "edit.html.erbに遷移すること" do
-      good = create(:good)
-      get :edit, params: { id: good }
+      good = create(:good, seller_id: user.id, category_id: category.id)
+      get :edit, params: { id: good}
       expect(response).to render_template :edit
     end
   end
