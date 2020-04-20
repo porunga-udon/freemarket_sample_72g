d1a234f0b22abc43dd8f06647ce1118f613abece
hideOz <sabokura@gmail.com>
2020-04-20 12:48:35 +0900

    Update good.rb

--- app/models/good.rb
+++ app/models/good.rb
@@ -21,7 +21,7 @@ class Good < ApplicationRecord
   validates :state,                 presence: true
   validates :region,                presence: true
   validates :postage,               presence: true
-  validates :expanation,            presence: true, length: { maximum: 1000 }
+  validates :expanation,            presence: true, length: { maximum: 400 }
   validates :shipping_date,         presence: true
   validates :delivery_method_id,    presence: true
   validates :category_id,           presence: true
